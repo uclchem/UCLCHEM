ce25bdc83ab8b9b17944a063e36312e90d55dfe0
jonholdship <jrh@star.ucl.ac.uk>
2020-03-25 18:11:52 +0000

    added sublimation to wrap.f90

--- src/wrap.f90
+++ src/wrap.f90
@@ -185,34 +185,32 @@ SUBROUTINE General(dictionary, outSpeciesIn)
 
     !loop until the end condition of the model is reached
     DO WHILE ((switch .eq. 1 .and. density(1) < finalDens) .or. (switch .eq. 0 .and. timeInYears < finalTime))
-
         !store current time as starting point for each depth step
-        IF (points .gt. 1) THEN
-            currentTimeold=targetTime
-            currentTime=currentTimeold
-        END IF
+        currentTimeold=currentTime
+
         !Each physics module has a subroutine to set the target time from the current time
         CALL updateTargetTime
 
         !loop over parcels, counting from centre out to edge of cloud
         DO dstep=1,points
-
-            density=abund(nspec+1,dstep)
-            !update physics
-            CALL updatePhysics
-            !update chemistry
+            !update chemistry from currentTime to targetTime
             CALL updateChemistry
 
-            !set time to the final time of integrator rather than target
-            targetTime=currentTime
-            !reset target for next depth point
-            if (points .gt. 1)currentTime=currentTimeold
-            !get time in years for output
+            currentTime=targetTime
+            !get time in years for output, currentTime is now equal to targetTime
             timeInYears= currentTime/SECONDS_PER_YEAR
-            !write this depth step
+
+            !Update physics so it's correct for new currentTime and start of next time step
+            CALL updatePhysics
+            !Sublimation checks if Sublimation should happen this time step and does it
+            CALL sublimation(abund)
+            !write this depth step now time, chemistry and physics are consistent
             CALL output
+
+            !reset time for next depth point
+            if (points .gt. 1)currentTime=currentTimeold
         END DO
-    END DO
+    END DO 
     close(10)
     close(11)
     close(7)
