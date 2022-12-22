bb33fcc8207566bc81d21258b6d6e09d4bae9ddd
Gijs Vermariën <vermarien@strw.leidenuniv.nl>
2022-12-22 14:55:01 +0100

    Refactored the Reaction class
    Added getters and setters
    Updated docstrings

--- src/uclchem/makerates/reaction.py
+++ src/uclchem/makerates/reaction.py
@@ -26,34 +26,37 @@ reaction_types = [
 class Reaction:
     def __init__(self, inputRow):
         try:
-            self.reactants = [
-                inputRow[0].upper(),
-                inputRow[1].upper(),
-                self.NANCheck(inputRow[2]).upper(),
-            ]
-            self.products = [
-                inputRow[3].upper(),
-                self.NANCheck(inputRow[4]).upper(),
-                self.NANCheck(inputRow[5]).upper(),
-                self.NANCheck(inputRow[6]).upper(),
-            ]
-            self.alpha = float(inputRow[7])
-            self.beta = float(inputRow[8])
-            self.gamma = float(inputRow[9])
-            self.templow = float(inputRow[10])
-            self.temphigh = float(inputRow[11])
+            self.set_reactants(
+                [
+                    inputRow[0].upper(),
+                    inputRow[1].upper(),
+                    self.NANCheck(inputRow[2]).upper(),
+                ]
+            )
+            self.set_products(
+                [
+                    inputRow[3].upper(),
+                    self.NANCheck(inputRow[4]).upper(),
+                    self.NANCheck(inputRow[5]).upper(),
+                    self.NANCheck(inputRow[6]).upper(),
+                ]
+            )
+            self.set_alpha(float(inputRow[7]))
+            self.set_beta(float(inputRow[8]))
+            self.set_gamma(float(inputRow[9]))
+            self.set_templow(float(inputRow[10]))
+            self.set_temphigh(float(inputRow[11]))
         except IndexError as error:
             raise ValueError(
                 "Input for Reaction should be a list of length 12"
             ) from error
-        self.reac_type = self.get_reaction_type()
         self.duplicate = False
 
         # body_count is the number of factors of density to include in ODE
         # we drop a factor of density from both the LHS and RHS of ODES
         # So reactions with 1 body have no factors of density which we manage by counting from -1
         self.body_count = -1
-        for reactant in self.reactants:
+        for reactant in self.get_reactants():
             if (reactant not in reaction_types) and reactant != "NAN":
                 self.body_count += 1
             if reactant in ["DESOH2", "FREEZE"]:
@@ -61,31 +64,174 @@ class Reaction:
             if reactant in ["LH", "LHDES"]:
                 self.body_count -= 1
 
-        if (self.get_reaction_type() == "FREEZE") and (self.reactants[0][-1] == "+"):
+        if (self.get_reaction_type() == "FREEZE") and (
+            self.get_reactants()[0][-1] == "+"
+        ):
             self.beta = 1
 
+    # Simple getters and setters for parsing the inputrow or changing parameters
+
+    def get_reactants(self) -> list[str]:
+        """Get the four reactants present in the reaction, padded with NAN for nonexistent
+
+        Returns:
+            list[str]: The four reactants names
+        """
+        return self._reactants[:]
+
+    def set_reactants(self, reactants: list[str]) -> None:
+        """Set the four reactants present in the reaction, padded with NAN for nonexistent
+
+        Args:
+            reactants (list[str]): The four reactants names
+        """
+        self._reactants = reactants
+
+    def get_products(self) -> list[str]:
+        """Get the four products present in the reaction, padded with NAN for nonexistent
+
+        Args:
+            reactants (list[str]): The four products names
+        """
+        return self._products[:]
+
+    def set_products(self, products: list[str]) -> None:
+        """Set the four products present in the reaction, padded with NAN for nonexistent
+
+        Args:
+            products (list[str]): The four products names
+        """
+        self._products = products
+
+    def get_alpha(self) -> float:
+        """Get the alpha parameter from the Kooij-Arrhenius equation
+
+        Returns:
+            float: the alpha parameter of the reaction
+        """
+        return self._alpha
+
+    def set_alpha(self, alpha: float) -> None:
+        """Set the alpha parameter from the Kooij-Arrhenius equation
+
+        Args:
+            alpha (float): the alpha parameter of the reaction
+        """
+        self._alpha = alpha
+
+    def get_beta(self) -> float:
+        """Get the beta parameter from the Kooij-Arrhenius equation
+
+        Returns:
+            float: the beta parameter of the reaction
+        """
+        return self._beta
+
+    def set_beta(self, beta: float) -> None:
+        """Set the beta parameter from the Kooij-Arrhenius equation
+
+        Args:
+            beta (float): the beta parameter of the reaction
+        """
+        self._beta = beta
+
+    def set_gamma(self, gamma: float) -> None:
+        """Set the gamma parameter from the Kooij-Arrhenius equation
+
+        Args:
+            gamma (float): the gamma parameter of the reaction
+        """
+        self._gamma = gamma
+
+    def get_gamma(self) -> float:
+        """Get the gamma  parameter from the Kooij-Arrhenius equation
+
+        Returns:
+            float: the gamma parameter of the reaction
+        """
+        return self._gamma
+
+    def set_templow(self, templow: float) -> None:
+        """Set the lower temperature boundary of the reaction in Kelvin
+
+        Args:
+            templow (float): the lower temperature boundary
+        """
+        self._templow = templow
+
+    def get_templow(self) -> float:
+        """Get the lower temperature boundary of the reaction in Kelvin
+
+        Returns:
+            float: the lower temperature boundary
+        """
+        return self._templow
+
+    def set_temphigh(self, temphigh: float) -> None:
+        """Set the higher temperature boundary of the reaction in Kelvin
+
+        Args:
+            templow (float): the higher temperature boundary
+        """
+        self._temphigh = temphigh
+
+    def get_temphigh(self) -> float:
+        """Get the higher temperature boundary of the reaction in Kelvin
+
+        Returns:
+            float: the higher temperature boundary
+        """
+        return self._temphigh
+
+    ## C
+
     def NANCheck(self, a):
-        aa = a if a else "NAN"
-        return aa
+        """Convert any Falsy statement to a NAN string
 
-    def get_reaction_type(self):
-        if self.reactants[2] in reaction_types:
-            return self.reactants[2]
+        Args:
+            a: thing to check for falsiness
+
+        Returns:
+            bool: input a if truthy, otherwise NAN
+        """
+        return a if a else "NAN"
+
+    def get_reaction_type(self) -> str:
+        """Get the type of a reaction from the reactants
+        First check the third reactant for a reaction type, then the second. If there are none
+        in there, it will be regarded as a two body reaction.
+
+        Returns:
+            str:
+        """
+        if self.get_reactants()[2] in reaction_types:
+            return self.get_reactants()[2]
+        elif self.get_reactants()[1] in reaction_types:
+            return self.get_reactants()[1]
         else:
-            if self.reactants[1] in reaction_types:
-                return self.reactants[1]
-            else:
-                return "TWOBODY"
+            return "TWOBODY"
 
-    def convert_to_bulk(self):
-        for i in range(len(self.reactants)):
-            self.reactants[i] = self.reactants[i].replace("#", "@")
-        for i in range(len(self.products)):
-            self.products[i] = self.products[i].replace("#", "@")
+    def convert_to_bulk(self) -> None:
+        """Convert the surface species to bulk species in place for this reaction."""
+        self.set_reactants([reac.replace("#", "@") for reac in self.get_reactants()])
+        self.set_products([prod.replace("#", "@") for prod in self.get_products()])
 
-    def __eq__(self, other):
-        if set(self.reactants) == set(other.reactants):
-            if set(self.products) == set(other.products):
+    def __eq__(self, other) -> bool:
+        """Check for equality against another reaction based on the products and reactants.
+        Note that it does not check for the temperature ranges that the reactions might have!
+
+        Args:
+            other: Another reaction set.
+
+        Returns:
+            bool: equality
+        """
+        if not isinstance(other, Reaction):
+            raise NotImplementedError(
+                "Equality is not implemented for anything but comparing to other reactions."
+            )
+        if set(self.get_reactants()) == set(other.get_reactants()):
+            if set(self.get_products()) == set(other.get_products()):
                 return True
         return False
 
@@ -94,19 +240,22 @@ class Reaction:
         This checks whether a grain reaction changes number of particles on the surface
         2 reactants to 2 products won't but two reactants combining to one will.
         """
-        if len([x for x in self.reactants if "#" in x]) != len(
-            [x for x in self.products if "#" in x]
+        if len([x for x in self.get_reactants() if "#" in x]) != len(
+            [x for x in self.get_products() if "#" in x]
         ):
             return True
-        if len([x for x in self.reactants if "@" in x]) != len(
-            [x for x in self.products if "@" in x]
+        if len([x for x in self.get_reactants() if "@" in x]) != len(
+            [x for x in self.get_products() if "@" in x]
         ):
             return True
         return False
 
     def changes_total_mantle(self):
+        """Check if the total grains on the mantle are changed by the reaction."""
         # If it's not just a movement between ice phases
-        if ("BULK" not in self.reactants[1]) and ("SWAP" not in self.reactants[1]):
+        if ("BULK" not in self.get_reactants()[1]) and (
+            "SWAP" not in self.get_reactants()[1]
+        ):
             # if the number of ice species changes
             if self.changes_surface_count():
                 return True
@@ -115,7 +264,7 @@ class Reaction:
         else:
             return False
 
-    def generate_ode_bit(self, i, species_names, three_phase):
+    def generate_ode_bit(self, i: int, species_names: list, three_phase: bool):
         """Every reaction contributes a fixed rate of change to whatever species it
         affects. We create the string of fortran code describing that change here.
 
@@ -130,7 +279,7 @@ class Reaction:
             ode_bit = ode_bit + f"*D"
 
         # then bring in factors of abundances
-        for species in self.reactants:
+        for species in self.get_reactants():
             if species in species_names:
                 ode_bit += f"*Y({species_names.index(species)+1})"
             elif species == "BULKSWAP":
@@ -143,31 +292,32 @@ class Reaction:
                     ode_bit = ode_bit + f"*Y({species_names.index('H')+1})"
             elif (species in ["THERM"]) and not (three_phase):
                 ode_bit += f"*D/safeMantle"
-            if "H2FORM" in self.reactants:
+            if "H2FORM" in self.get_reactants():
                 # only 1 factor of H abundance in Cazaux & Tielens 2004 H2 formation so stop looping after first iteration
                 break
 
-        if "LH" in self.reactants[2]:
-            if "@" in self.reactants[0]:
+        if "LH" in self.get_reactants()[2]:
+            if "@" in self.get_reactants()[0]:
                 ode_bit += "*bulkLayersReciprocal"
         self.ode_bit = ode_bit
 
     def __str__(self):
         return (
-            " + ".join(filter(lambda r: r != "NAN", self.reactants))
+            " + ".join(filter(lambda r: r != "NAN", self.get_reactants()))
             + " -> "
-            + " + ".join(filter(lambda p: p != "NAN", self.products))
+            + " + ".join(filter(lambda p: p != "NAN", self.get_products()))
         )
 
     def __repr__(self):
         return (
-            self.reac_type
+            self.get_reaction_type()
             + " reaction: "
             + " + ".join(
                 filter(
-                    lambda r: (r != "NAN") and (r not in reaction_types), self.reactants
+                    lambda r: (r != "NAN") and (r not in reaction_types),
+                    self.get_reactants(),
                 )
             )
             + " -> "
-            + " + ".join(filter(lambda p: p != "NAN", self.products))
+            + " + ".join(filter(lambda p: p != "NAN", self.get_products()))
         )
