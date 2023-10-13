317399e9843cd71050f1b6c54fe57ef25484c6d1
Gijs Vermariën <gijs@vermarien.com>
2023-10-13 11:23:14 +0200

    Updated the compare scripts to newest makerates (#44)
    Updated the scripts to compare the species, reactions and networks with the newest version of Makerates (>3.2.0)

--- Makerates/comparespecies.py
+++ Makerates/comparespecies.py
@@ -4,13 +4,13 @@ from uclchem.makerates.io_functions import read_species_file
 
 
 speciesFile = "inputFiles/default_species.csv"
-speciesFile2 = "inputFiles/test_default_species.csv"
+speciesFile2 = "inputFiles/excited_species.csv"
 
-species_list = read_species_file(speciesFile)
-species_list2 = read_species_file(speciesFile2)
+species_list, _ = read_species_file(speciesFile)
+species_list2, _ = read_species_file(speciesFile2)
 
-print(len(species_list))
-print(len(species_list2))
+print(f"File {speciesFile} contains {len(species_list)} species.")
+print(f"File {speciesFile2} contains {len(species_list2)} species.")
 
 print(f"Species in {speciesFile2} but not {speciesFile}")
 missing = [x for x in species_list2 if x not in species_list]
