05841cafac7a12185cd53cddcc1292d5abfeb51a
Gijs Vermariën <gijs@vermarien.com>
2023-10-19 14:47:52 +0200

    Pytest fixes, Path fix and faster pip install (#47)
    * Version numbers are strings not floats
    
    * Change trigger to on pull_request
    
    * We moved away from requirements.txt sometime ago.
    
    * It would be nice to install pytest
    
    * Fixed triple calls from setup.py
    
    Fixes #41
    
    * Added Path for the output_dir
    
    No longer string / string

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-import setuptools  # this is the "magic" import
 import subprocess
 import sys
 
@@ -9,17 +8,20 @@ from shutil import move
 
 #make clean every time slows things but is needed because users will mostly install after changing networks
 #and a make never seems to full update the new network
-cwd = os.getcwd()
-os.chdir('src/fortran_src/')
-result = subprocess.call('make clean', shell=True)
-# if result != 0:
-#     exit(1)
-result = subprocess.call('make python', shell=True)
-if result != 0:
-    exit(1)
-os.chdir(cwd)
+# print(f"STAGE: {sys.argv[1]}, full command: {sys.argv}")
+if sys.argv[1] in ("editable_wheel"):
+    cwd = os.getcwd()
+    os.chdir('src/fortran_src/')
+    _ = subprocess.call('make clean', shell=True)
+    result = subprocess.call('make python', shell=True)
+    if result != 0:
+        exit(1)
+    os.chdir(cwd)
 
-wrap_file = glob("src/fortran_src/uclchemwrap*.so")[0]
-move(wrap_file, "src/uclchem/uclchemwrap.so")
-
+    wrap_file = glob("src/fortran_src/uclchemwrap*.so")[0]
+    move(wrap_file, "src/uclchem/uclchemwrap.so")
+elif sys.argv[1] in ("bdist_wheel"):
+    raise RuntimeError("Cannot install UCLCHEM in the site packages, use `pip install -e .`")
+else:
+    pass
 setup()
