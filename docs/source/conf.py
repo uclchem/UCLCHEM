89f8d7b577001fd9a7ce6a8e8c73efbaae72d1e5
Gijs Vermariën <gijs@vermarien.com>
2024-09-16 14:12:47 +0200

    Add apidoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,6 +17,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.apidoc'
 ]
 
 intersphinx_mapping = {
@@ -33,3 +34,15 @@ html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+apidoc_module_dir = '../../src/uclchem'
+apidoc_output_dir = 'reference'
+apidoc_excluded_paths = ['tests']
+apidoc_separate_modules = True
+
+
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../../src/uclchem'))
