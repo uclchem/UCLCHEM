852ad776579e2d3354c31aba39c0db36c70c81be
Tobias Dijkhuis <tobiasdijkhuis@hotmail.com>
2026-08-23 13:31:56 +0200

    Add some testing for chemistry.f90

---
+++ tests/test_chemistry.py
@@ -0,0 +1,36 @@
+import uclchemwrap
+from uclchem.advanced.runtime_network import RuntimeNetwork
+
+import numpy as np
+import uclchem.constants
+import pandas as pd
+from uclchem.analysis import total_element_abundance
+import pytest
+
+
+def test_get_ionlist():
+    network = RuntimeNetwork()
+    species_list = network.get_species_list()
+    ion_list, n_ions = uclchemwrap.chemistry.get_ion_indices()
+
+    assert np.count_nonzero(ion_list) == n_ions
+    for ion_index in ion_list:
+        if ion_index != 0:
+            assert species_list[ion_index - 1].is_ion()
+
+
+def test_get_total_elemental_abundance():
+    rng = np.random.default_rng()
+    abundances = rng.random(size=uclchem.constants.n_species)
+    columns = [name.decode("utf-8").strip() for name in uclchemwrap.network.specname]
+    abundances_df = pd.DataFrame([abundances], columns=columns)
+
+    elemental_abundances_fortran = uclchemwrap.chemistry.get_total_elemental_abundances(
+        abundances
+    )
+    for idx, element in enumerate(uclchemwrap.network.elem_names):
+        element = element.decode("utf-8").strip()
+
+        assert total_element_abundance(
+            element, abundances_df
+        ).to_numpy() == pytest.approx(elemental_abundances_fortran[idx])
